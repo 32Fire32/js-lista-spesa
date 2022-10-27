2cb565c9888687268ef2046d6401acbf347adcdd
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-27 15:12:52 +0200

    correzzione posizione i++

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 'strict mode'
 
 const shoppingList = [
-    'pannolini extra',
-    'salviette',
+    'pane',
+    'pasta',
     'pannolini',
-    'ciucci',
     'pappette',
     'giocattoli'
 ]
@@ -15,10 +14,10 @@ const listContainer = document.getElementById("list");
 
 let i = 0;
 
-while ( i < shoppingList.length) {
-    i++;
+while ( i < shoppingList.length ) {
     console.log(i);
     let listItem = document.createElement("li");
     listItem.innerHTML = shoppingList[i];
     listContainer.append(listItem);
+    i++;
 }
